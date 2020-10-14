3f506337098ff3f98c4ca263c2661a230d3a0648
Gabriel Barreto <gabriel.devloper@gmail.com>
2020-10-14 08:12:31 -0300

    criando componente do orfanato

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,8 +1,32 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiPlus } from 'react-icons/fi';
+
+import mapMakerImg from '../images/map-maker.svg';
 
 const OrphanagesMap: React.FC = () => {
   return (
-    <h1>Teste</h1>
+    <div id="page-map">
+      <aside>
+        <header>
+          <img src={mapMakerImg} alt="Happy"/>
+
+          <h2>Escolha um orfanato no mapa</h2>
+          <p>Muitas crianças estão esperando sua visita :)</p>
+        </header>
+
+        <footer>
+          <strong>Maceió</strong>
+          <span>Alagoas</span>
+        </footer>
+      </aside>
+
+      <div></div>
+
+      <Link to="" className="create-orphanage">
+        <FiPlus size={32}/>
+      </Link>
+    </div>
   );
 }
 
